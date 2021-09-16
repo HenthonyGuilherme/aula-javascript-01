82f7f3f05ba0eb4acda60ca2d46cfa1ddfe7a99d
Henthony Guilherme <henthonyguilhermeferreira.2021@gmail.com>
2021-09-16 19:30:24 -0300

    <+_+>

---
+++ atividade16-09.js
@@ -0,0 +1,60 @@
+//Tipo de dado no Javascript: 
+
+//Number --------------------------------------
+var number = 13;
+console.log(number);
+
+//String ---------------------------------
+var string = 'Ç_Ç 0lá';
+console.log(string);
+
+//Boolean --------------------------
+var boolean = true;
+console.log("gosta de bolo?");
+console.log(boolean);
+
+//Array  ------------------------------
+var array = ["dois", 2];
+console.log (array);
+
+//Object  ------------------------------
+var object = {caderno: "vermelho", ehPlastico: false, quantidade: 2};
+console.log(object);
+console.log(object.quantidade);
+
+//Undefined   ----------------------
+var udefined;
+console.log("A variavel estar: " + udefined);
+
+//Null    ---------------------------
+var vazia = null;
+console.log("Avariaveu esta: " + vazia);
+
+
+// Declaração de variáveis no Javascript.
+
+// Var ---------------------
+var nome = "Henthony";
+console.log(nome);
+
+nome = "Clau";
+console.log(nome);
+
+var numero = 101001;
+console.log(numero);
+
+// Let ---------------------
+let idade = 20;
+console.log(idade);
+
+idade = 35;
+console.log(idade);
+
+let conta = "R$: 120.00";
+
+// Const -----------------------
+const cpf = 77788800099;
+console.log(cpf);
+
+const numeroOuro = 1.61803399;
+console.log(numeroOuro);
